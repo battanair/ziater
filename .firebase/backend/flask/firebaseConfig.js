b0478c42d7c30b52295839c3a49dfd70906f8d43
Eduardo Battaner <edu@MacBook-Pro-de-Eduardo.local>
2025-02-06 20:21:50 +0100

    intento de backend

---
+++ .firebase/backend/flask/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCz61R5v83aUEgX_p2jwy_XjbdypGTK8Gc",
+    authDomain: "ziater2.firebaseapp.com",
+    projectId: "ziater2",
+    storageBucket: "ziater2.firebasestorage.app",
+    messagingSenderId: "442908543941",
+    appId: "1:442908543941:web:4b6320865b60d3ee1619ac"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app); // Create the auth object
+
+export { auth }; // Export the auth object
+
+
+
+
+
